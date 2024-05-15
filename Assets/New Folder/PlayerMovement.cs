bab6814a6b715ff51c6b0d2fc8a9cbfd637a75a8
muchothepanda <161780716+muchothepanda@users.noreply.github.com>
2024-05-15 16:53:26 -0700

    Finished movement

--- Assets/New Folder/PlayerMovement.cs
+++ Assets/New Folder/PlayerMovement.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float move;
+    public float speed;
+    public float jump;
+    
+    public bool Isgrounded;
+
+    
     void Start()
     {
-        
+       rb= GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        Jump();
     }
+
+    private void Movement()
+    {
+        move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (move* speed, rb.velocity.y);
+       
+           
+       
+    }
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && (Isgrounded==false))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Isgrounded = false;
+        }
+    }
+    
+    
+    
 }
