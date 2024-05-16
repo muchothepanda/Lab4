30b3cde6a3baba381cb512c30b859dfe9114738b
muchothepanda <161780716+muchothepanda@users.noreply.github.com>
2024-05-16 11:56:38 -0700

    Finished block

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public GameObject breakable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit");
+            Destroy(breakable);
+        }
+    }
+}
